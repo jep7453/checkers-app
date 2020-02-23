4e341ecf49a02e27dabd450d24d5b8977d036526
Jonathan Pofcher <jep7453@rit.edu>
2020-02-23 17:43:23 -0500

    Added the GET /signin route to the WebServer

--- src/main/java/com/webcheckers/ui/WebServer.java
+++ src/main/java/com/webcheckers/ui/WebServer.java
@@ -54,6 +54,10 @@ public class WebServer {
    */
   public static final String HOME_URL = "/";
 
+  /**
+   * The URL pattern to request the Sign in Page.
+   */
+  public static final String SIGN_IN_URL = "/signin";
   //
   // Attributes
   //
@@ -138,6 +142,8 @@ public class WebServer {
 
     // Shows the Checkers game Home page.
     get(HOME_URL, new GetHomeRoute(templateEngine));
+    // Shows the Sign In page.
+    get(SIGN_IN_URL, new GetSignInRoute(templateEngine));
 
     //
     LOG.config("WebServer is initialized.");
