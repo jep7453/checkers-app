6fb17fa1076837b0ed4b1d00c1c491628c0ee177
Jonathan Pofcher <jep7453@rit.edu>
2020-02-23 19:08:22 -0500

    Made POST /signinname, to accept a username and have it display on the homepage
    // NOT WORKING FULLY PROPERLY

---
+++ src/main/java/com/webcheckers/ui/PostSignInNameRoute.java
@@ -0,0 +1,72 @@
+package com.webcheckers.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+import com.webcheckers.util.Message;
+
+/**
+ * The UI Controller to POST the SignIn page.
+ *
+ * @author <a href='mailto:bdbvse@rit.edu'>Bryan Basham</a>
+ */
+public class PostSignInNameRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+
+    private static final Message WELCOME_MSG = Message.info("Welcome to the world of online Checkers.");
+
+    private final TemplateEngine templateEngine;
+
+    private static final String USER_NAME = "userName";
+    private static final String USER_PARAM = "myUserName";
+
+    /**
+     * Create the Spark Route (UI controller) to handle all {@code GET /} HTTP requests.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     */
+    public PostSignInNameRoute(final TemplateEngine templateEngine) {
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        //
+        LOG.config("PostSignInNameRoute is initialized.");
+    }
+
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     *
+     * @return
+     *   the rendered HTML for the Home page
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetHomeRoute is invoked.");
+        //
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!");
+
+
+
+        // retrieve username
+        final String userName = request.queryParams(USER_PARAM);
+
+        // display a user message in the Home page
+        vm.put("message", Message.info(userName));
+
+        // render the View
+        return templateEngine.render(new ModelAndView(vm , "home.ftl"));
+    }
+}
