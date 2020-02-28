71c82bad8cbb6b38af78d3de030055fd410d85d6
kl3468 <kl3468@g.rit.edu.in>
2020-02-28 12:41:01 -0500

    Starting the get game route.

---
+++ src/main/java/com/webcheckers/ui/GetGameRoute.java
@@ -0,0 +1,18 @@
+package com.webcheckers.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+import com.webcheckers.util.Message;
+public class GetGameRoute  {
+
+
+}
