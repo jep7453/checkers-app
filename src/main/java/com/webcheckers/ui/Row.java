a8c024dcf85e2d3035ad930847d224c2c54d7cf9
kl3468 <kl3468@g.rit.edu.in>
2020-02-29 10:29:50 -0500

    Updating all the UI Tier classes to accomodate the checker board. Finished most of the Get Game Route.

--- src/main/java/com/webcheckers/ui/Row.java
+++ src/main/java/com/webcheckers/ui/Row.java
@@ -1,5 +1,7 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.model.Checkerboard;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,21 +16,21 @@ public class Row implements Iterable<Space> {
     private List<Space> spaces;
 
 
-/**
- * Definning the constructor which intialises the a space on the board with the cell index.
- * This method creates the object **/
-    public Row(int index) {
+    /**
+     * Definning the constructor which intialises the a space on the board with the cell index.
+     * This method creates the object **/
+    public Row(int index, Checkerboard checkerboard) {
         this.index = index;
         spaces = new ArrayList<Space>();
 
-        spaces.add(new Space(0,index));
-        spaces.add(new Space(1,index));
-        spaces.add(new Space(2,index));
-        spaces.add(new Space(3,index));
-        spaces.add(new Space(4,index));
-        spaces.add(new Space(5,index));
-        spaces.add(new Space(6,index));
-        spaces.add(new Space(7,index));
+        spaces.add(new Space(0,index,checkerboard));
+        spaces.add(new Space(1,index,checkerboard));
+        spaces.add(new Space(2,index,checkerboard));
+        spaces.add(new Space(3,index,checkerboard));
+        spaces.add(new Space(4,index,checkerboard));
+        spaces.add(new Space(5,index,checkerboard));
+        spaces.add(new Space(6,index,checkerboard));
+        spaces.add(new Space(7,index,checkerboard));
     }
 
     public Iterator<Space> iterator() {
