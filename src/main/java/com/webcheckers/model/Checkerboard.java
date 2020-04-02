ecf2018a2e7535f1afcf829306220aed5420cde8
cst1465 <cst1465@rit.edu>
2020-04-02 10:55:53 -0400

    created algorithm to check if a piece can move

--- src/main/java/com/webcheckers/model/Checkerboard.java
+++ src/main/java/com/webcheckers/model/Checkerboard.java
@@ -71,16 +71,84 @@ public class Checkerboard {
     // define temporary ranks and file to edit
     int tempRank, tempFile;
 
-    // check for a simple move to the left
-    //tempRank = rank + 1;
-    //tempFile = file - 1;
+    /*
+    X -> the space jumping to
+    C -> the piece being moved
+    J -> the square being jumped
+    This algorithm first checks for a simple jump move, in either direction. And if it is a
+    king then it it checks behind as well
+    |   |   |   |   |   |
+    |   | X |   | X |   |
+    |   |   | C |   |   |
+    |   | X |   | X |   |
+    |   |   |   |   |   |
 
+    It then checks to see if a jump move is possible by going two spaces diagonally
+    and checking the space being jumped isn't the same color as the current checker
+    | X |   |   |   | X |
+    |   | J |   | J |   |
+    |   |   | C |   |   |
+    |   | J |   | J |   |
+    | X |   |   |   | X |
+     */
+    for(int i = 1; i < 3; i++){
+      // calculate space being jumped to
+      tempRank = rank + i;
+      tempFile = file + i;
 
+      // move diagonally upwards and to the right
+      if(canMoveHelper(tempRank, tempFile, tempRank-1, tempFile-1,
+              (i==2), checker.getColor()))
+        return (true);
 
+      // move diagonally upwards and to the left
+      if(canMoveHelper(tempRank,-tempFile, tempRank-1, tempFile+1,
+              (i==2), checker.getColor()))
+        return (true);
 
+      // move diagonally DOWNWARDS and to the right
+      if(isKing && canMoveHelper(-tempRank, tempFile,-tempRank+1, tempFile-1,
+              (i==2), checker.getColor()))
+        return (true);
 
+      // move diagonally DOWNWARDS and to the left
+      if(isKing && canMoveHelper(-tempRank, -tempFile, -tempRank+1, -tempFile + 1,
+              (i==2), checker.getColor()))
+        return (true);
+    }
 
-    return (true);
+    return (false);
+  }
+
+  /**
+   * This algorithm checks to see if a piece can move to a certain spot.
+   * There are two cases, checking for a simple move and checking for a jump move
+   * Simple move:
+   *     - Checks to make sure space the piece want to move to is on the board
+   *     - if there is not a square there then return true
+   * Jump move:
+   *     - Checks to make sure it is possible to jump is possible without going off board
+   *     - Checks to see if the checker that is being jumped over is the opposing color
+   *
+   * @param tempRank the rank attempting to jump to
+   * @param tempFile the file attemption to jump to
+   * @param jumpRank rank of a piece if its getting jumped
+   * @param jumpFile file of a piece if it is getting jumped
+   * @param jumpMove if there is a jump move
+   * @param col the color of the current checker
+   * @return of the piece can move to that square
+   */
+  private boolean canMoveHelper(int tempRank, int tempFile, int jumpRank, int jumpFile, boolean jumpMove, Checker.Color col){
+    if(0 <= tempRank && tempRank < NUM_RANKS && 0 <= tempFile && tempFile < NUM_FILES ){
+      if(jumpMove){
+        Checker checker = squares[jumpRank][jumpFile].getChecker();
+        if(checker.getColor().equals(col))
+          return (false);
+      }
+      if(!squares[tempRank][tempFile].hasChecker())
+        return (true);
+    }
+    return (false);
   }
 
 }
