2cb781fe1844ff4c71da314d4947ab9613431349
cst1465 <cst1465@rit.edu>
2020-04-02 13:06:45 -0400

    started unit testing pieces in moving situation

--- src/test/java/com/webcheckers/ui/NoMovesTest.java
+++ src/test/java/com/webcheckers/ui/NoMovesTest.java
@@ -1,9 +1,9 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.model.Checkerboard;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 
@@ -13,7 +13,8 @@ public class NoMovesTest {
     private Checkerboard checkerboard;
 
     @Test
-    public void cantMove(){
+    public void cantMoveSingle(){
         checkerboard = new Checkerboard("noMovesSingle.txt");
+        assertFalse(checkerboard.pieceCanMove(checkerboard.getSquare(3,4)));
     }
 }
